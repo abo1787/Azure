4cdcb9c7a21e9ff3993821122e829f619880f8b7
djpierson <67014458+djpierson@users.noreply.github.com>
2020-11-20 09:17:38 +0000

    Update setLocaleUk.ps1

--- WVD/Image/CustomScriptExtensions/setLocaleUk.ps1
+++ WVD/Image/CustomScriptExtensions/setLocaleUk.ps1
@@ -1,155 +1,47 @@
-[CmdletBinding(SupportsShouldProcess = $true)]
-param (
-    
-    [string] $osVersion,
-    [string] $ExecutableName = "en-GB.zip"
-)
 
-#####################################
-
-# Helper #
-##########
-#region Functions
-function LogInfo($message) {
-    Log "Info" $message
-}
-
-function LogError($message) {
-    Log "Error" $message
-}
-
-function LogSkip($message) {
-    Log "Skip" $message
-}
-function LogWarning($message) {
-    Log "Warning" $message
-}
-
-function Log {
-
-    <#
-    .SYNOPSIS
-   Creates a log file and stores logs based on categories with tab seperation
-    .PARAMETER category
-    Category to put into the trace
-    .PARAMETER message
-    Message to be loged
-    .EXAMPLE
-    Log 'Info' 'Message'
-    #>
-
-    Param (
-        $category = 'Info',
-        [Parameter(Mandatory = $true)]
-        $message
-    )
-
-    $date = get-date
-    $content = "[$date]`t$category`t`t$message`n"
-    Write-Verbose "$content" -verbose
-
-    if (! $script:Log) {
-        $File = Join-Path $env:TEMP "log.log"
-        Write-Error "Log file not found, create new $File"
-        $script:Log = $File
-    }
-    else {
-        $File = $script:Log
-    }
-    Add-Content $File $content -ErrorAction Stop
-}
-
-function Set-Logger {
-    <#
-    .SYNOPSIS
-    Sets default log file and stores in a script accessible variable $script:Log
-    Log File name "executionCustomScriptExtension_$date.log"
-    .PARAMETER Path
-    Path to the log file
-    .EXAMPLE
-    Set-Logger
-    Create a logger in
-    #>
-
-    Param (
-        [Parameter(Mandatory = $true)]
-        $Path
-    )
-
-    # Create central log file with given date
-
-    $date = Get-Date -UFormat "%Y-%m-%d %H-%M-%S"
-
-    $scriptName = (Get-Item $PSCommandPath ).Basename
-    $scriptName = $scriptName -replace "-", ""
-
-    Set-Variable logFile -Scope Script
-    $script:logFile = "executionCustomScriptExtension_" + $scriptName + "_" + $date + ".log"
-
-    if ((Test-Path $path ) -eq $false) {
-        $null = New-Item -Path $path -type directory
-    }
-
-    $script:Log = Join-Path $path $logfile
-
-    Add-Content $script:Log "Date`t`t`tCategory`t`tDetails"
-}
-#endregion
-
-$mylogfile = "C:\WindowsAzure\Logs\Plugins\Microsoft.Compute.CustomScriptExtension\userlog.txt"
-
-Add-Content $mylogfile -Value "Starting script"
-Add-Content $mylogfile -Value "PSScriptRoot is $PSScriptRoot"
-
-Set-Logger "C:\WindowsAzure\Logs\Plugins\Microsoft.Compute.CustomScriptExtension\executionLog\UKLocale" # inside "executionCustomScriptExtension_$scriptName_$date.log"
-
-# Language codes
+# Set variables
 $PrimaryLanguage = "en-GB"
+$filePath = "C:\Windows\Temp\setLocaleUk"
+$LangPackName = "en-GB\LanguageExperiencePack.en-GB.Neutral.appx"
+$LangPackPath = Join-Path $filePath $LangPackName
+$LicenseName = "en-GB\License.xml"
+$LicensePath = Join-Path $filePath $LicenseName
 
-# Check osVersion to set correct Language Pack version
-if($osVersion -eq "19h2-evd" -or $osVersion -eq "19h2-ent"){
-    $Uri = "https://raw.githubusercontent.com/Bistech/Azure/master/WVD/Image/LangPacks/1909/en-GB.zip"
-}
-else{
-    $Uri = "https://raw.githubusercontent.com/Bistech/Azure/master/WVD/Image/LangPacks/2004/en-GB.zip"
-}
 
-# Get Local Experience Pack
-    Invoke-WebRequest -Uri $Uri -OutFile "$($PSScriptRoot)\$ExecutableName"
-    $LangArchivePath = Join-Path $PSScriptRoot "en-GB.zip"
-    $LangPackName = "en-GB\LanguageExperiencePack.en-GB.Neutral.appx"
-    $LangPackPath = Join-Path $PSScriptRoot $LangPackName
-    $LicenseName = "en-GB\License.xml"
-    $LicensePath = Join-Path $PSScriptRoot $LicenseName
-    
-    Add-Content $mylogfile -Value "Downloaded language packs"
-    Add-Content $mylogfile -Value "LangArchivePath is $LangArchivePath"
-    Add-Content $mylogfile -Value "LangPackPath is $LangPackPath"
-    Add-Content $mylogfile -Value "LicensePath is $LicensePath"
-    Add-Content $mylogfile -Value "Primary Language is $PrimaryLanguage"
-    
 # Provision Local Experience Pack
-    Unblock-File –Path $LangArchivePath –ErrorAction SilentlyContinue
-    Expand-Archive -Path $LangArchivePath -DestinationPath $PSScriptRoot
+try{
     Add-AppxProvisionedPackage –Online –PackagePath $LangPackPath –LicensePath $LicensePath
-    Add-Content $mylogfile -Value "Added AppXPackage"
+}
+catch{
+    Write-Output "Error - Couldn't install AppXPackage" -ErrorAction Stop
+}
 
 # Install optional features for primary language
 $UKCapabilities = Get-WindowsCapability –Online | Where-Object {$_.Name -match "$PrimaryLanguage" -and $_.State -ne "Installed"}
 $UKCapabilities | ForEach-Object {
     Add-WindowsCapability –Online –Name $_.Name
-    Add-Content $mylogfile -Value "Adding capability $($_.Name)"
+    Write-Output "Adding capability $($_.Name)"
 }
 
 $LanguageList = Get-WinUserLanguageList
-$LanguageList.Add("en-gb")
+$LanguageList.Add("en-GB")
 Set-WinUserLanguageList $LanguageList -Force
+
+# Set Languages/Culture
+Set-Culture en-GB
+Set-WinSystemLocale en-GB
+Set-WinHomeLocation -GeoId 242
+Set-WinUserLanguageList en-GB -Force
 
 # Get xml File
 $xmlFile = "setLocaleUk.xml"
-$xmlUri = "https://raw.githubusercontent.com/Bistech/Azure/master/WVD/Image/CustomScriptExtensions/setLocaleUk.xml"
-Invoke-WebRequest -Uri $xmlUri -OutFile "$($PSScriptRoot)\$xmlFile"
-$xmlPath = Join-Path $PSScriptRoot $xmlFile
-Add-Content $mylogfile -Value "Downloading XML file"
-Add-Content $mylogfile -Value "XML Path is $xmlPath"
+$xmlPath = Join-Path $filePath $xmlFile
 
+# Set Language Admin Defaults
+$Process = Start-Process –FilePath Control.exe –ArgumentList "intl.cpl,,/f:`"$xmlPath`"" –NoNewWindow –PassThru –Wait
+$Process.ExitCode
+
+# Set Timezone
+& tzutil /s "GMT Standard Time"
+
+Restart-Computer -Force
