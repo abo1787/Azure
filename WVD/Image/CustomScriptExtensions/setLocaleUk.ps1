4a58d113b36191415624373f5f667c7b37f6fdac
djpierson <67014458+djpierson@users.noreply.github.com>
2020-11-20 12:16:46 +0000

    Update setLocaleUk.ps1

--- WVD/Image/CustomScriptExtensions/setLocaleUk.ps1
+++ WVD/Image/CustomScriptExtensions/setLocaleUk.ps1
@@ -9,7 +9,7 @@ $LicensePath = Join-Path $filePath $LicenseName
 
 # Provision Local Experience Pack
 try{
-    Write-Output "Adding en-GB Language Pack"
+    Write-Output "Adding en-GB Language Pack.."
     Add-AppxProvisionedPackage -Online -PackagePath $LangPackPath -LicensePath $LicensePath
 }
 catch{
@@ -19,7 +19,7 @@ catch{
 # Install optional features for primary language
 $UKCapabilities = Get-WindowsCapability -Online | Where-Object {$_.Name -match "$PrimaryLanguage" -and $_.State -ne "Installed"}
 $UKCapabilities | ForEach-Object {
-    Write-Output "Adding capability $($_.Name)"
+    Write-Output "Adding capability $($_.Name).."
     try{
         Add-WindowsCapability -Online -Name $_.Name
     }
@@ -28,11 +28,14 @@ $UKCapabilities | ForEach-Object {
     }
 }
 
+# Set Language List
+Write-Output "Setting Language list.."
 $LanguageList = Get-WinUserLanguageList
 $LanguageList.Add("en-GB")
 Set-WinUserLanguageList $LanguageList -Force
 
 # Set Languages/Culture
+Write-Output "Setting cultures to UK.."
 Set-Culture en-GB
 Set-WinSystemLocale en-GB
 Set-WinHomeLocation -GeoId 242
@@ -43,10 +46,23 @@ $xmlFile = "setLocaleUk.xml"
 $xmlPath = Join-Path $filePath $xmlFile
 
 # Set Language Admin Defaults
+Write-Output "Setting default Languages to UK.."
 $Process = Start-Process -FilePath Control.exe -ArgumentList "intl.cpl,,/f:`"$xmlPath`"" -NoNewWindow -PassThru -Wait
 $Process.ExitCode
 
 # Set Timezone
+Write-Output "Setting timezone to GMT.."
 & tzutil /s "GMT Standard Time"
 
-Restart-Computer -Force
+# Cleanup files
+Write-Output "Cleaning up files.."
+$desktopLink = "C:\Users\Public\Desktop\setLocaleUk.lnk"
+Remove-Item -Path $xmlPath -Force
+Remove-Item -Path "$filePath\en-GB" -Force -Recurse
+Remove-Item -Path "$filePath\en-GB.zip" -Force -Recurse
+Remove-Item $MyInvocation.MyCommand.Source
+Remove-Item $filePath -Force -Recurse
+Remove-Item $desktopLink -Force
+
+Write-Output "All settings have been updated. Restarting computer in 5 seconds"
+Start-Sleep 5 ; Restart-Computer -Force
