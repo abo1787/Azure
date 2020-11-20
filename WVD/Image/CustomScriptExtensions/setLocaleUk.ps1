92a8ce52ceb3089a5187df78b540c2e1f00bd426
djpierson <67014458+djpierson@users.noreply.github.com>
2020-11-20 12:35:41 +0000

    Update setLocaleUk.ps1

--- WVD/Image/CustomScriptExtensions/setLocaleUk.ps1
+++ WVD/Image/CustomScriptExtensions/setLocaleUk.ps1
@@ -1,3 +1,6 @@
+# Set the ExecutionPolicy
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force -Confirm:$false
+
 # Set variables
 $PrimaryLanguage = "en-GB"
 $filePath = "C:\Windows\Temp\setLocaleUk"
