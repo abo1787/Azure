aa6cc79daf55a9b6ef972ef430e3d9f6b867eb6f
djpierson <67014458+djpierson@users.noreply.github.com>
2021-06-02 12:46:46 +0100

    Delete installEvergreen.ps1

--- WVD/Image/CustomScriptExtensions/installEvergreen.ps1
+++
@@ -1,7 +0,0 @@
-# Install Evergreen
-Install-Module -Name Evergreen -Confirm:$false -Force
-Import-Module -Name Evergreen
-
-# Create directory for software download
-$dirPath = "C:\Windows\Temp"
-New-Item -Path $dirPath -Name "evergreenSoftware" -ItemType Directory
