82aadebadcbbe8979f9068147b34d9f31143eceb
djpierson <67014458+djpierson@users.noreply.github.com>
2021-06-03 13:37:31 +0100

    Update installEvergreen.ps1

--- WVD/Image/CustomScriptExtensions/Evergreen/installEvergreen.ps1
+++ WVD/Image/CustomScriptExtensions/Evergreen/installEvergreen.ps1
@@ -1,3 +1,14 @@
+# Do not prompt user for confirmations
+Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
+
+# Install PackageManagement
+Write-Output "Installing PackageManagement"
+Install-Package -Name PackageManagement -MinimumVersion 1.4.7 -Force -Confirm:$false -Source PSGallery
+
+# Install PowershellGet
+Write-Output "Installing PowershellGet"
+Install-Package -Name PowershellGet -Force
+
 # Install Evergreen
 Install-Module -Name Evergreen -Confirm:$false -Force
 Import-Module -Name Evergreen
