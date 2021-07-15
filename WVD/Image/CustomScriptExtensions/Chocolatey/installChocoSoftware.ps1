0ad9c4991fbf0d9ff6d2118d635e997cfc7e8fdd
djpierson <67014458+djpierson@users.noreply.github.com>
2021-07-15 09:12:46 +0100

    Create installChocoSoftware.ps1

---
+++ WVD/Image/CustomScriptExtensions/Chocolatey/installChocoSoftware.ps1
@@ -0,0 +1,18 @@
+# Set the ExecutionPolicy
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force -Confirm:$false
+
+# Set path for software instruction file
+$softwarePath = "C:\Windows\Temp\aibVendorSoftwareToInstall.csv"
+
+# Import software list to install
+Write-Output "Installing packages"
+$software = Import-Csv -Path $softwarePath
+foreach ($package in $software) {
+
+    Choco Install $package.Name -y
+    Write-Output "Installed package $($package.Name)"
+    
+}
+
+# Remove software instruction file
+Remove-Item $softwarePath
