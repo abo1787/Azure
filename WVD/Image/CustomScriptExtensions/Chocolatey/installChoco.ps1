212a63a5ee6b539bcf99a02d830c37746c536a30
djpierson <67014458+djpierson@users.noreply.github.com>
2021-07-15 14:23:46 +0100

    Update installChoco.ps1

--- WVD/Image/CustomScriptExtensions/Chocolatey/installChoco.ps1
+++ WVD/Image/CustomScriptExtensions/Chocolatey/installChoco.ps1
@@ -1,8 +1,2 @@
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-# Install Chocolatey PowerShell module
-Install-Module -Name chocolatey -Force
-
-# Import Chocolatey PowerShell module
-Import-Module chocolatey
