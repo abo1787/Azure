6648b55b6b6e0a124e6a24fa2eec0e1206561922
djpierson <67014458+djpierson@users.noreply.github.com>
2021-07-27 15:19:33 +0100

    Update installWingetSoftware.ps1

--- WVD/Image/CustomScriptExtensions/Winget/installWingetSoftware.ps1
+++ WVD/Image/CustomScriptExtensions/Winget/installWingetSoftware.ps1
@@ -4,12 +4,21 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force -Confirm:$
 # Set path for software instruction file
 $softwarePath = "C:\Windows\Temp\aibVendorSoftwareToInstall.csv"
 
+# Change directory
+Set-Location C:\Users\packer\AppData\Local\Microsoft\WindowsApps
+
+# Output for troubleshooting
+$location = Get-Location
+Write-Output "Directory is $location"
+$enviromentPath = $env:Path -split ';'
+Write-Output "Environment path is $enviromentPath"
+
 # Import software list to install
 Write-Output "Installing packages"
 $software = Import-Csv -Path $softwarePath
 foreach ($package in $software) {
 
-    C:\Users\packer\AppData\Local\Microsoft\WindowsApps\winget.exe winget install --id $package.Id -h
+    .\winget.exe install --id $package.Id -h
     Write-Output "Installed package $package"
     
 }
