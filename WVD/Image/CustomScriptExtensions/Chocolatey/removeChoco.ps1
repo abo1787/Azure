895b47ecea710a71f6f06312b3e3309d34b5ed13
djpierson <67014458+djpierson@users.noreply.github.com>
2021-07-29 12:40:51 +0100

    Create removeChoco.ps1

---
+++ WVD/Image/CustomScriptExtensions/Chocolatey/removeChoco.ps1
@@ -0,0 +1,59 @@
+# Set the ExecutionPolicy
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force -Confirm:$false
+
+# Uninstall Chocolatey PowerShell module
+Uninstall-Module -Name chocolatey -Force
+
+# Remove Chocolatey
+$userKey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Environment')
+$userPath = $userKey.GetValue('PATH', [string]::Empty, 'DoNotExpandEnvironmentNames').ToString()
+
+$machineKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SYSTEM\ControlSet001\Control\Session Manager\Environment\')
+$machinePath = $machineKey.GetValue('PATH', [string]::Empty, 'DoNotExpandEnvironmentNames').ToString()
+
+if ($userPath -like "*$env:ChocolateyInstall*") {
+    Write-Output "Chocolatey Install location found in User Path. Removing..."
+
+    $newUserPATH = @(
+        $userPath -split [System.IO.Path]::PathSeparator |
+        Where-Object { $_ -and $_ -ne "$env:ChocolateyInstall\bin" }
+    ) -join [System.IO.Path]::PathSeparator
+
+    $userKey.SetValue('PATH', $newUserPATH, 'ExpandString')
+}
+
+if ($machinePath -like "*$env:ChocolateyInstall*") {
+    Write-Output "Chocolatey Install location found in Machine Path. Removing..."
+
+    $newMachinePATH = @(
+        $machinePath -split [System.IO.Path]::PathSeparator |
+        Where-Object { $_ -and $_ -ne "$env:ChocolateyInstall\bin" }
+    ) -join [System.IO.Path]::PathSeparator
+
+    $machineKey.SetValue('PATH', $newMachinePATH, 'ExpandString')
+}
+
+# Adapt for any services running in subfolders of ChocolateyInstall
+$agentService = Get-Service -Name chocolatey-agent -ErrorAction SilentlyContinue
+if ($agentService -and $agentService.Status -eq 'Running') {
+    $agentService.Stop()
+}
+
+Remove-Item -Path $env:ChocolateyInstall -Recurse -Force
+
+'ChocolateyInstall', 'ChocolateyLastPathUpdate' | ForEach-Object {
+    foreach ($scope in 'User', 'Machine') {
+        [Environment]::SetEnvironmentVariable($_, [string]::Empty, $scope)
+    }
+}
+
+$machineKey.Close()
+$userKey.Close()
+
+if ($env:ChocolateyToolsLocation -and (Test-Path $env:ChocolateyToolsLocation)) {
+    Remove-Item -Path $env:ChocolateyToolsLocation -Recurse -Force
+}
+
+foreach ($scope in 'User', 'Machine') {
+    [Environment]::SetEnvironmentVariable('ChocolateyToolsLocation', [string]::Empty, $scope)
+}
