45869e807246dc3300425d1140c85b3886244ac0
djpierson <67014458+djpierson@users.noreply.github.com>
2021-08-03 12:37:24 +0100

    Update azureImageBuilderSysprep.ps1

--- WVD/Image/CustomScriptExtensions/azureImageBuilderSysprep.ps1
+++ WVD/Image/CustomScriptExtensions/azureImageBuilderSysprep.ps1
@@ -1,5 +1,11 @@
+# Write to AIB Output
+Write-Output "*** STARTING CUSTOM SYSPREP DOWNLOAD ***"
+
 $customSysprepUrl = "https://raw.githubusercontent.com/Bistech/Azure/master/WVD/Image/CustomScriptExtensions/DeprovisioningScript.ps1"
 $customSysprepFilePath = "C:\DeprovisioningScript.ps1"
 
 # Download custom sysprep script
 Invoke-WebRequest -Uri $customSysprepUrl -OutFile $customSysprepFilePath -UseBasicParsing
+
+# Write to AIB Output
+Write-Output "*** COMPLETED CUSTOM SYSPREP DOWNLOAD ***"
