c142ba9a46e42b599805c77033151c72d6d2a51b
djpierson <67014458+djpierson@users.noreply.github.com>
2021-08-03 12:32:07 +0100

    Update installChoco.ps1

--- WVD/Image/CustomScriptExtensions/Chocolatey/installChoco.ps1
+++ WVD/Image/CustomScriptExtensions/Chocolatey/installChoco.ps1
@@ -1,2 +1,8 @@
+# Write to AIB Output
+Write-Output "*** STARTING CHOCOLATEY INSTALL ***"
+
 # Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Write to AIB Output
+Write-Output "*** COMPLETED CHOCOLATEY INSTALL ***"
