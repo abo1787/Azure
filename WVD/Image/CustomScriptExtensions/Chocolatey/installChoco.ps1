d9cb0a3ad63475e0841fd9115ee864768c3d76a5
djpierson <67014458+djpierson@users.noreply.github.com>
2021-08-03 16:30:40 +0100

    Update installChoco.ps1

--- WVD/Image/CustomScriptExtensions/Chocolatey/installChoco.ps1
+++ WVD/Image/CustomScriptExtensions/Chocolatey/installChoco.ps1
@@ -1,8 +1,10 @@
 # Write to AIB Output
-Write-Output "*** STARTING CHOCOLATEY INSTALL ***"
+$timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+Write-Output "$timeStamp *** STARTING CHOCOLATEY INSTALL ***"
 
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Write to AIB Output
-Write-Output "*** COMPLETED CHOCOLATEY INSTALL ***"
+$timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+Write-Output "$timeStamp *** COMPLETED CHOCOLATEY INSTALL ***"
