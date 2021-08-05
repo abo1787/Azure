045a2d40d2e34268e252452c7506c182ff244803
djpierson <67014458+djpierson@users.noreply.github.com>
2021-08-05 13:23:28 +0100

    Update removeVMsFromDomain.ps1

--- WVD/Hostpool/removeVMsFromDomain.ps1
+++ WVD/Hostpool/removeVMsFromDomain.ps1
@@ -1,8 +1,10 @@
 param(
     $domainUser,
-    $domainPass)
+    $domainPass
+)
 
-
+# Create PSCredential Object
 $creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $domainUser, $domainPass
 
+# Remove computer from domain
 Remove-Computer -UnjoinDomainCredential $creds -WorkgroupName "WORKGROUP" -Restart -Force
