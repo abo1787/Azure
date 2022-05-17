761086641a14f9e25e4386bb4eab37c3d342ff8f
djpierson <67014458+djpierson@users.noreply.github.com>
2022-05-17 16:09:15 +0100

    Update setLocaleUkAIB.ps1

--- WVD/Image/CustomScriptExtensions/setLocaleUkAIB.ps1
+++ WVD/Image/CustomScriptExtensions/setLocaleUkAIB.ps1
@@ -42,7 +42,6 @@ Write-Output "Setting cultures to UK.."
 Set-Culture en-GB
 Set-WinSystemLocale en-GB
 Set-WinHomeLocation -GeoId 242
-Set-WinUserLanguageList en-GB -Force
 
 # Get xml File
 $xmlFile = "setLocaleUkAIB.xml"
