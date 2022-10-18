06636ab68f953f95846a93bab97ac47d1f22f5e0
Dave Pierson <djpierson@me.com>
2022-10-18 10:33:40 +0100

    Removed custom logger

--- WVD/Image/CustomScriptExtensions/installFSLogix.ps1
+++ WVD/Image/CustomScriptExtensions/installFSLogix.ps1
@@ -1,116 +1,30 @@
 [CmdletBinding(SupportsShouldProcess = $true)]
 param (
     
-    [Parameter(Mandatory = $false)]
-    [ValidateNotNullOrEmpty()]
-    [string] $ExecutableName = "FSLogixApp.zip"
+  [Parameter(Mandatory = $false)]
+  [ValidateNotNullOrEmpty()]
+  [string] $executableName = "FSLogixApp.zip"
 
 )
 
-#####################################
+# Download installer
+Write-Output "Downloading installer..."
+$uri = "https://download.microsoft.com/download/d/1/9/d190de51-f1c1-4581-9007-24e5a812d6e9/FSLogix_Apps_2.9.8228.50276.zip"
+Invoke-WebRequest -Uri $Uri -OutFile "$($PSScriptRoot)\$executableName"
+$fsLogixArchivePath = Join-Path $PSScriptRoot "FSLogixApp.zip"
 
-##########
-# Helper #
-##########
-#region Functions
-function LogInfo($message) {
-    Log "Info" $message
-}
+# Extract installer
+Write-Output "Extracting installer..."
+Expand-Archive -Path $fsLogixArchivePath -DestinationPath $PSScriptRoot
 
-function LogError($message) {
-    Log "Error" $message
-}
+# Set paths & switches
+$executableName = "x64\Release\FSLogixAppsSetup.exe"
+$fsLogixExePath = Join-Path $PSScriptRoot $executableName
+$switches = "/passive /norestart"
 
-function LogSkip($message) {
-    Log "Skip" $message
-}
-function LogWarning($message) {
-    Log "Warning" $message
-}
+# Install
+Write-Output "Installing Microsoft Teams from path '$fsLogixExePath'"
+$installer = Start-Process -FilePath $fsLogixExePath -ArgumentList $switches -Wait -PassThru
 
-function Log {
-
-    <#
-    .SYNOPSIS
-   Creates a log file and stores logs based on categories with tab seperation
-    .PARAMETER category
-    Category to put into the trace
-    .PARAMETER message
-    Message to be loged
-    .EXAMPLE
-    Log 'Info' 'Message'
-    #>
-
-    Param (
-        $category = 'Info',
-        [Parameter(Mandatory = $true)]
-        $message
-    )
-
-    $date = get-date
-    $content = "[$date]`t$category`t`t$message`n"
-    Write-Verbose "$content" -verbose
-
-    if (! $script:Log) {
-        $File = Join-Path $env:TEMP "log.log"
-        Write-Error "Log file not found, create new $File"
-        $script:Log = $File
-    }
-    else {
-        $File = $script:Log
-    }
-    Add-Content $File $content -ErrorAction Stop
-}
-
-function Set-Logger {
-    <#
-    .SYNOPSIS
-    Sets default log file and stores in a script accessible variable $script:Log
-    Log File name "executionCustomScriptExtension_$date.log"
-    .PARAMETER Path
-    Path to the log file
-    .EXAMPLE
-    Set-Logger
-    Create a logger in
-    #>
-
-    Param (
-        [Parameter(Mandatory = $true)]
-        $Path
-    )
-
-    # Create central log file with given date
-
-    $date = Get-Date -UFormat "%Y-%m-%d %H-%M-%S"
-
-    $scriptName = (Get-Item $PSCommandPath ).Basename
-    $scriptName = $scriptName -replace "-", ""
-
-    Set-Variable logFile -Scope Script
-    $script:logFile = "executionCustomScriptExtension_" + $scriptName + "_" + $date + ".log"
-
-    if ((Test-Path $path ) -eq $false) {
-        $null = New-Item -Path $path -type directory
-    }
-
-    $script:Log = Join-Path $path $logfile
-
-    Add-Content $script:Log "Date`t`t`tCategory`t`tDetails"
-}
-#endregion
-
-Set-Logger "C:\WindowsAzure\Logs\Plugins\Microsoft.Compute.CustomScriptExtension\executionLog\FSLogix" # inside "executionCustomScriptExtension_$scriptName_$date.log"
-
-$Uri = "https://download.microsoft.com/download/d/1/9/d190de51-f1c1-4581-9007-24e5a812d6e9/FSLogix_Apps_2.9.8228.50276.zip"
-Invoke-WebRequest -Uri $Uri -OutFile "$($PSScriptRoot)\$ExecutableName"
-$FSLogixArchivePath = Join-Path $PSScriptRoot "FSLogixApp.zip"
-Expand-Archive -Path $FSLogixArchivePath -DestinationPath $PSScriptRoot
-
-$ExecutableName = "x64\Release\FSLogixAppsSetup.exe"
-$FSLogixExePath = Join-Path $PSScriptRoot $ExecutableName
-
-$Switches = "/passive /norestart"
-
-$Installer = Start-Process -FilePath $FSLogixExePath -ArgumentList $Switches -Wait -PassThru
-
-LogInfo("The exit code is $($Installer.ExitCode)")
+# Output
+Write-Output "The exit code is $($installer.ExitCode)"
